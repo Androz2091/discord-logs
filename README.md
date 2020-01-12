6025ff53a175ad92661df91f1f8ac1c1852f5a84
Androz2091 <androz2091@gmail.com>
2020-01-12 16:18:06 +0100

    :tada: Init repository

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
-# discord-logs
- 📝Framework that simplify the usage of advanced Discord logs
+# Discord Logs
+
+Discord Logs is a powerful package which add many useful events to your Discord.js client!
+
+**Warning**: This package uses recent Discord.js features and only works on v12 or higher (`npm install discordjs/discord.js`).
+
+```js
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const logs = require('discord-logs');
+logs(client);
+
+// Access to new events, like guildMemberRoleAdd!
+client.on('guildMemberRoleAdd', (member, role) => {
+    console.log(`${member.user.tag} now has the role ${role.name}!`);
+});
+
+client.login('YOUR_DISCORD_BOT_TOKEN');
+```
+
+## Event List
+
+You will find here all new events that Discord Logs implement. Not all events that could be supported are supported by the module. You can open a pull request to add new ones.
+
+### Member Events
+
+* `guildMemberRoleAdd`: emitted when a member receives a new role
+
+```js
+// When a role is added to a member
+client.on('guildMemberRoleAdd', (member, role) => {
+    console.log(`${member.user.tag} now has the role ${role.name}!`);
+});
+```
+
+* `guildMemberRoleRemove`: emitted when a member loses a role
+
+```js
+// When a role is removed from a member
+client.on('guildMemberRoleRemove', (member, role) => {
+    console.log(`${member.user.tag} lost the role ${role.name}!`);
+});
+```
+
+* `guildMemberNicknameUpdate`: emitted when a member's nickname is updated
+```js
+// When a member nickname changes
+client.on('guildMemberNicknameUpdate', (oldMember, newMember) => {
+    console.log(`${newMember.user.tag}'s nickname is now ${newMember.nickname}!`);
+});
+```
+
+### User Events
+
+* `userAvatarUpdate`: emitted when a user avatar changes
+  
+```js
+// When a user avatar changes
+client.on('userAvatarUpdate', (oldUser, newUser) => {
+    console.log(`${newMember.user.tag}'s avatar updated!`);
+});
+```
+
+* `userUsernameUpdate`: emitted when a user username changes
+  
+```js
+// When a user username changes
+client.on('userUsernameUpdate', (oldUser, newUser) => {
+    console.log(`${oldUser.user.username} is now ${newUser.user.username}!`);
+});
+```
+
+### Voice State Events
+
+* `voiceChannelJoin`: emitted when a member joins a voice channel
+
+```js
+// When a member joins a voice channel
+client.on('voiceChannelJoin', (oldMember, newMember) => {
+    console.log(`${newMember.user.tag} joined the voice channel ${newMember.voice.channel.name}!`);
+});
+```
+
+* `voiceChannelLeave`: emitted when a member leaves a voice channel
+
+```js
+// When a member leaves a voice channel
+client.on('voiceChannelLeave', (oldMember, newMember) => {
+    console.log(`${oldMember.user.tag} left the voice channel ${oldMember.voice.channel.name}!`);
+});
+```
+
+* `voiceChannelSwitch`: emitted when a member switches to another voice channel
+
+```js
+// When a member switches to another voice channel
+client.on('voiceChannelSwitch', (oldMember, newMember) => {
+    console.log(`${oldMember.user.tag} left the voice channel ${oldMember.voice.channel.name} and joined ${newMember.voice.channel.name}!`);
+});
+```
+
+* `voiceChannelMute`: emitted when a member become muted
+
+```js
+// When a member become muted
+client.on('voiceChannelMute', (oldMember, newMember) => {
+    console.log(`${oldMember.user.tag} is now muted!`);
+});
+```
+
+* `voiceChannelUnmute`: emitted when a member become unmuted
+
+```js
+// When a member become unmuted
+client.on('voiceChannelMute', (oldMember, newMember) => {
+    console.log(`${oldMember.user.tag} is now unmuted!`);
+});
+```
---
+++ index.js
@@ -0,0 +1,97 @@
+/**
+ * Handles the guildMemberUpdate event and emits the specific event
+ * @param {Client} client The Discord Client instance
+ * @param {GuildMember} oldMember The member without the change
+ * @param {GuildMember} newMember The member with the change
+ */
+const handleGuildMemberUpdateEvent = (client, oldMember, newMember) => {
+    let addedRole = null;
+    // Check if a role was added
+    newMember.roles.forEach((role) => {
+        if(!oldMember.roles.has(role.id)){
+            addedRole = role;
+        }
+    });
+    if(addedRole){
+        return client.emit('guildMemberRoleAdd', oldMember, addedRole)
+    }
+    // Check if a role was removed
+    let removedRole = null;
+    oldMember.roles.forEach((role) => {
+        if(!newMember.roles.has(role.id)){
+            removedRole = role;
+        }
+    });
+    if(removedRole){
+        return client.emit('guildMemberRoleRemove', oldMember, removedRole)
+    }
+    // Check if the nickname has changed
+    if(oldMember.nickname !== newMember.nickname){
+        return client.emit('guildMemberNicknameUpdate', oldMember, newMember);
+    }
+};
+
+/**
+ * Handles the userUpdate event and emits the specific event
+ * @param {Client} client The Discord Client instance
+ * @param {User} oldUser The user without the change
+ * @param {User} newUser The user with the change
+ */
+const handleUserUpdateEvent = (client, oldUser, newUser) => {
+    // Check if the avatar has changed
+    if(oldUser.displayAvatarURL !== newUser.displayAvatarURL){
+        return client.emit('userAvatarUpdate', oldUser, newUser);
+    }
+    // Check if the username has changed
+    if(oldUser.username !== newUser.username){
+        return client.emit('userUsernameUpdate', oldUser, newUser);
+    }
+};
+
+/**
+ * Handles the voiceStateUpdate event and emits the specific event
+ * @param {Client} client The Discord Client instance
+ * @param {GuildMember} oldMember The member without the change
+ * @param {GuildMember} newMember The member with the change
+ */
+const handleVoiceStateUpdateEvent = (client, oldMember, newMember) => {
+    // If the member joins the voice channel
+    if(!oldMember.voice.channel && newMember.voice.channel){
+        return client.emit('voiceChannelJoin', oldMember, newMember);
+    }
+    // If the member leaves the voice channel
+    if(oldMember.voice.channel && !newMember.voice.channel){
+        return client.emit('voiceChannelLeave', oldMember, newMember);
+    }
+    // If the member changes the voice channel
+    if(oldMember.voice.channel && newMember.voice.channel && (oldMember.voice.channel.id !== newMember.voice.channel.id)){
+        return client.emit('voiceChannelSwitch', oldMember, newMember);
+    }
+    // If the member became muted
+    if(!oldMember.voice.mute && newMember.voice.mute){
+        return client.emit('voiceChannelMute', oldMember, newMember);
+    }
+    // If the member became unmuted
+    if(oldMember.voice.mute && !newMember.voice.mute){
+        return client.emit('voiceChannelUnmute', oldMember, newMember);
+    }
+};
+
+module.exports = async (client) => {
+
+    /* HANDLE MEMBER EVENTS */
+    client.on('guildMemberUpdate', (oldMember, newMember) => {
+        handleGuildMemberUpdateEvent(client, oldMember, newMember);
+    });
+
+    /* HANDLE USER EVENTS */
+    client.on('userUpdate', (oldUser, newUser) => {
+        handleUserUpdateEvent(client, oldUser, newUser);
+    });
+
+    /* HANDLE VOICE STATE UPDATE */
+    client.on('voiceStateUpdate', (oldMember, newMember) => {
+        handleVoiceStateUpdateEvent(client, oldMember, newMember);
+    });
+
+};
