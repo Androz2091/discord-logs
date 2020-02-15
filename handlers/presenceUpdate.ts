35223cd28af4adeabb5bee5764105977a319856b
Androz2091 <androz2091@gmail.com>
2020-02-15 17:51:13 +0100

    fix: check if oldPresence exists

--- handlers/presenceUpdate.ts
+++ handlers/presenceUpdate.ts
@@ -1,6 +1,11 @@
 import { Client, Presence } from 'discord.js';
 
-export async function handlePresenceUpdateEvent(client: Client, oldPresence: Presence, newPresence: Presence) {
+export async function handlePresenceUpdateEvent(
+    client: Client,
+    oldPresence: Presence | null | undefined,
+    newPresence: Presence,
+) {
+    if (!oldPresence) return;
     // Check if the member is now online
     if (oldPresence.status === 'offline' && newPresence.status !== 'offline') {
         return client.emit('guildMemberOnline', oldPresence.member, newPresence.member);
