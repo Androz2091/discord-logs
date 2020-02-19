25c253e7fb46b873828bbda9a7a50a4236ed2e47
Androz <androz2091@gmail.com>
2020-02-19 12:03:06 +0100

    Rename handler function in guildUpdate.ts

--- handlers/guildUpdate.ts
+++ handlers/guildUpdate.ts
@@ -1,6 +1,6 @@
 import { Client, Guild} from 'discord.js';
 
-export async function handleGuildMemberUpdateEvent(client: Client, guild: Guild) {
+export async function handleGuildUpdateEvent(client: Client, guild: Guild) {
     // If the guild increased in boost level
     if (!guild.premiumTier && guild.premiumTier) {
         client.emit('guildBoostLevelUp', guild);
