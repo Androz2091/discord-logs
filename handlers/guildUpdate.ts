39759572617829ee550f69dd1f722a563cf86b13
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-02-19 12:10:15 +0100

    Add guildBoostLevelUp event (#2)
    * Create guildUpdate.ts
    
    * Export guildUpdate handler
    
    * Handle guildUpdate event
    
    * Rename handler function in guildUpdate.ts
    
    * Update guildUpdate.ts
    
    Co-authored-by: Androz <androz2091@gmail.com>

---
+++ handlers/guildUpdate.ts
@@ -0,0 +1,8 @@
+import { Client, Guild } from 'discord.js';
+
+export async function handleGuildUpdateEvent(client: Client, oldGuild: Guild, newGuild: Guild) {
+    // If the guild increased in boost level
+    if (oldGuild.premiumTier < newGuild.premiumTier) {
+        client.emit('guildBoostLevelUp', oldGuild, newGuild);
+    }
+ }
