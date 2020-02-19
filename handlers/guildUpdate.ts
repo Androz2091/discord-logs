f9cceecec3fc7088c96ae29f634b5fc21969e6b0
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-02-19 12:45:39 +0100

    Update guildUpdate.ts

--- handlers/guildUpdate.ts
+++ handlers/guildUpdate.ts
@@ -5,4 +5,8 @@ export async function handleGuildUpdateEvent(client: Client, oldGuild: Guild, ne
     if (oldGuild.premiumTier < newGuild.premiumTier) {
         client.emit('guildBoostLevelUp', oldGuild, newGuild);
     }
+    // If the guild lowering in boost level
+    if (oldGuild.premiumTier > newGuild.premiumTier) {
+        client.emit('guildBoostLevelDown', oldGuild, newGuild);
+    }
 }
