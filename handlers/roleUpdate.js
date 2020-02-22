e825d946296af1e9630e70f4310ac4355f94d261
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-02-22 07:56:58 +0100

    Create roleUpdate.js

---
+++ handlers/roleUpdate.js
@@ -0,0 +1,20 @@
+import { Client, Role } from 'discord.js';
+
+/**
+ * @handler Role Events
+role */
+export async function handleRoleUpdateEvent(client: Client, oldRole: Role, newRole: Role) {
+    /**
+     * @event rolePositionUpdate
+     * @description Emitted when a role change position.
+     * @param {DJS:Role} role The role whose position has changed.
+     * @param {number} oldPosition The old role position.
+     * @param {number} newPosition The new role position.
+     * @example
+     * client.on("rolePositionUpdate", (role, oldPosition, newPosition) => {
+     *   console.log(role.name + "was at position "+oldPosition+"and now is at position"+newPosition);
+     * });
+     */
+    if (oldRole.rawPosition !== newRole.rawPosition) {
+        client.emit('rolePositionUpdate', newRole, oldRole.rawPosition, newRole.rawPosition);
+    }
