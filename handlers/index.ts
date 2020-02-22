ab47fb5cdab7c7eb69f3b030717722f12e414e79
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-02-22 13:18:11 +0400

    Update index.ts

--- handlers/index.ts
+++ handlers/index.ts
@@ -1,6 +1,7 @@
 export * from './guildMemberUpdate';
 export * from './guildUpdate';
 export * from './presenceUpdate';
+export * from './roleUpdate";
 export * from './userUpdate';
 export * from './voiceStateUpdate';
 export * from './messageUpdate';
