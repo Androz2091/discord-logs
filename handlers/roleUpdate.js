bc180212ad4829488fc991ba6f6c4e19319c93d3
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-02-22 13:10:10 +0400

    Update handlers/roleUpdate.js
    Co-Authored-By: Androz <androz2091@gmail.com>

--- handlers/roleUpdate.js
+++ handlers/roleUpdate.js
@@ -6,7 +6,7 @@ role */
 export async function handleRoleUpdateEvent(client: Client, oldRole: Role, newRole: Role) {
     /**
      * @event rolePositionUpdate
-     * @description Emitted when a role change position.
+     * @description Emitted when a role position changes.
      * @param {DJS:Role} role The role whose position has changed.
      * @param {number} oldPosition The old role position.
      * @param {number} newPosition The new role position.
