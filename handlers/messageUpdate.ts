9f52cff754cecf3f7d8159ab54544404204a3333
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-02-22 07:09:14 +0400

    Update handlers/messageUpdate.ts
    Co-Authored-By: Androz <androz2091@gmail.com>

--- handlers/messageUpdate.ts
+++ handlers/messageUpdate.ts
@@ -20,7 +20,7 @@ export async function handleMessageUpdateEvent(client: Client, oldMessage: Messa
      /**
      * @event messageEdited
      * @description Emitted when a message has been edited.
-     * @param {DJS:Message} message The message that was edited.
+     * @param {DJS:Message} oldMessage The message before it was edited.
      * @param {object} oldMessage The message before that was edited.
      * @param {object} newMessage The message after that was edited.
      * @example
