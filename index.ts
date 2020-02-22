a586cc2450e034bc97d2afd448d81188207b94dc
Androz2091 <androz2091@gmail.com>
2020-02-22 10:38:40 +0100

    style: remove unused space

--- index.ts
+++ index.ts
@@ -29,12 +29,12 @@ export = async (client: Client) => {
     client.on('presenceUpdate', (oldPresence: Presence, newPresence: Presence) => {
         handlePresenceUpdateEvent(client, oldPresence, newPresence);
     });
-    
+
     /* HANDLE ROLE EVENTS */
     client.on('roleUpdate', (oldRole: Role, newRole: Role) => {
         handleRoleUpdateEvent(client, oldRole, newRole);
     });
-    
+
     /* HANDLE USER EVENTS */
     client.on('userUpdate', (oldUser: User, newUser: User) => {
         handleUserUpdateEvent(client, oldUser, newUser);
