78cfc2ebaaffa9c33204c29961fb8923ce8525ca
Androz2091 <androz2091@gmail.com>
2020-02-22 14:24:31 +0100

    feat: add test generation

---
+++ scripts/generate-test.js
@@ -0,0 +1,45 @@
+const fs = require('fs').promises;
+const Comments = require('parse-comments');
+const comments = new Comments();
+
+const asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+};
+
+const botStart = `
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const logs = require("../");
+logs(client);
+
+client.on("ready", () => {
+    console.log("Ready. Logged as "+client.user.tag+" in "+client.guilds.cache.size+" servers.");
+});
+`;
+const botEnd = `
+client.login(process.env.TOKEN);
+`;
+let botContent = '';
+
+(async () => {
+    const handlersFiles = await fs.readdir('./handlers');
+    await asyncForEach(
+        handlersFiles.filter(f => f.split('.')[0] !== 'index'),
+        async handler => {
+            const handlerContent = await fs.readFile(`./handlers/${handler}`, 'utf-8');
+            const parsedComments = comments.parse(handlerContent);
+            const handlerTitle = parsedComments[0].tags.find(tag => tag.title === 'handler').description;
+            const handlerDescription = `Events related to the ${
+                parsedComments[0].tags.find(tag => tag.title === 'related').description
+            } event.`;
+            botContent += `\n/* ${handlerTitle} */\n// ${handlerDescription}`;
+            parsedComments.forEach(blockComment => {
+                botContent += '\n' + (blockComment.examples[0] ? blockComment.examples[0].value.slice(1) : '');
+            });
+        },
+    );
+    fs.writeFile('./tests/test.js', `${botStart}${botContent}${botEnd}`, 'utf-8');
+})();
---
+++ tests/test.js
@@ -0,0 +1,137 @@
+
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const logs = require("../");
+logs(client);
+
+client.on("ready", () => {
+    console.log("Ready. Logged as "+client.user.tag+" in "+client.guilds.cache.size+" servers.");
+});
+
+/* Guild Member Events */
+// Events related to the guildMemberUpdate event.
+
+client.on("guildMemberBoost", (member) => {
+  console.log(member.user.tag+" has started boosting "+member.guild.name+"!");
+});
+
+client.on("guildMemberBoost", (member) => {
+  console.log(member.user.tag+" has stopped boosting "+member.guild.name+"...");
+});
+
+client.on("guildMemberRoleAdd", (member, role) => {
+  console.log(member.user.tag+" acquired the role: "+role.name);
+});
+
+client.on("guildMemberRoleRemove", (member, role) => {
+  console.log(member.user.tag+" lost the role: "+role.name);
+});
+
+client.on("guildMemberNicknameUpdate", (member, oldNickname, newNickname) => {
+  console.log(member.user.tag+"'s nickname is now "+newNickname);
+});
+
+/* Guild Events */
+// Events related to the guildUpdate event.
+
+client.on("guildBoostLevelUp", (guild, oldLevel, newLevel) => {
+  console.log(guild.name+" reaches the boost level: "+newLevel);
+});
+
+client.on("guildBoostLevelDown", (guild, oldLevel, newLevel) => {
+  console.log(guild.name+" returned to the boost level: "+newLevel);
+});
+
+client.on("guildRegionUpdate", (guild, oldRegion, newRegion) => {
+  console.log(guild.name+" region is now "+newRegion);
+});
+
+client.on("guildBannerAdd", (guild, bannerURL) => {
+  console.log(guild.name+" has a banner now!");
+});
+
+client.on("guildAfkChannelAdd", (guild, afkChannel) => {
+  console.log(guild.name+" has an AFK channel now!");
+});
+
+/* Message Events */
+// Events related to the messageUpdate event.
+
+client.on("messagePinned", (message) => {
+  console.log("This message bas been pinned : "+message);
+});
+
+/* Presence Events */
+// Events related to the presenceUpdate event.
+
+client.on("guildMemberOffline", (member, oldStatus) => {
+  console.log(member.user.tag+" became offline!");
+});
+
+client.on("guildMemberOnline", (member, newStatus) => {
+  console.log(member.user.tag+" was offline and is now "+newStatus+"!");
+});
+
+/* Role Events */
+// Events related to the roleUpdate event.
+
+client.on("rolePositionUpdate", (role, oldPosition, newPosition) => {
+  console.log(role.name + " was at position "+oldPosition+" and now is at position "+newPosition);
+});
+
+/* User Events */
+// Events related to the userUpdate event.
+
+client.on("userAvatarUpdate", (user, oldAvatarURL, newAvatarURL) => {
+  console.log(user.tag+" avatar updated!");
+});
+
+client.on("userUsernameUpdate", (user, oldUsername, newUsername) => {
+  console.log(user.tag+" username updated!");
+});
+
+client.on("userUsernameUpdate", (user, oldDiscriminator, newDiscriminator) => {
+  console.log(user.tag+" discriminator updated!");
+});
+
+/* Voice Events */
+// Events related to the voiceStateUpdate event.
+
+client.on("voiceChannelJoin", (member, channel) => {
+  console.log(member.user.tag+" joined "+channel.name+"!");
+});
+
+client.on("voiceChannelLeave", (member, channel) => {
+  console.log(member.user.tag+" left "+channel.name+"!");
+});
+
+client.on("voiceChannelSwitch", (member, oldChannel, newChannel) => {
+  console.log(member.user.tag+" left "+oldChannel.name+" and joined "+newChannel.name+"!");
+});
+
+client.on("voiceChannelMute", (member, muteType) => {
+  console.log(member.user.tag+" become muted! (type: "+muteType);
+});
+
+client.on("voiceChannelMute", (member, oldMuteType) => {
+  console.log(member.user.tag+" become unmuted!");
+});
+
+client.on("voiceChannelDeaf", (member, deafType) => {
+  console.log(member.user.tag+" become deafed!");
+});
+
+client.on("voiceChannelUneaf", (member, deafType) => {
+  console.log(member.user.tag+" become undeafed!");
+});
+
+client.on("voiceStreamingStart", (member, voiceChannel) => {
+  console.log(member.user.tag+" started streaming in "+voiceChannel.name);
+});
+
+client.on("voiceStreamingStop", (member, voiceChannel) => {
+  console.log(member.user.tag+" stopped streaming");
+});
+
+client.login(process.env.TOKEN);
