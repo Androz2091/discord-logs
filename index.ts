d69f8dae3dbbe4b8512753fbdb4febb9b3c5e1a9
Mime <74558118+Mimexe@users.noreply.github.com>
2021-10-09 12:56:37 +0200

    Added "[discord-logs] [DEBUG]"
    Added "[discord-logs] [DEBUG]" in debug console log

--- index.ts
+++ index.ts
@@ -21,73 +21,73 @@ export = async (client: Client, options?: { debug?: boolean }) => {
 
     /* HANDLE GUILDS EVENTS */
     if (intents.has('GUILDS')) {
-        if (options?.debug) console.log('channelUpdate event handler registered.');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] channelUpdate event handler registered.');
         client.on('channelUpdate', (oldChannel, newChannel) => {
             handleChannelUpdateEvent(client, oldChannel, newChannel);
         });
-        if (options?.debug) console.log('guildUpdate event handler registered.');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] guildUpdate event handler registered.');
         client.on('guildUpdate', (oldGuild, newGuild) => {
             handleGuildUpdateEvent(client, oldGuild, newGuild);
         });
-        if (options?.debug) console.log('roleUpdate event handler registered.');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] roleUpdate event handler registered.');
         client.on('roleUpdate', (oldRole, newRole) => {
             handleRoleUpdateEvent(client, oldRole, newRole);
         });
-        if (options?.debug) console.log('threadUpdate event handler registered.');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] threadUpdate event handler registered.');
         client.on('threadUpdate', (oldThread, newThread) => {
             handleThreadChannelUpdateEvent(client, oldThread, newThread);
         });
     } else {
         if (options?.debug)
             console.log(
-                'channelUpdate, guildUpdate, roleUpdate and threadUpdate event handlers not registered (missing GUILDS intent).',
+                '[discord-logs] [DEBUG] channelUpdate, guildUpdate, roleUpdate and threadUpdate event handlers not registered (missing GUILDS intent).',
             );
     }
 
     /* HANDLE MEMBER EVENTS */
     if (intents.has('GUILD_MEMBERS')) {
-        if (options?.debug) console.log('guildMemberUpdate event handler registered.');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] guildMemberUpdate event handler registered.');
         client.on('guildMemberUpdate', (oldMember, newMember) => {
             handleGuildMemberUpdateEvent(client, oldMember, newMember);
         });
-        if (options?.debug) console.log('userUpdate event handler registered.');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] userUpdate event handler registered.');
         client.on('userUpdate', (oldUser, newUser) => {
             handleUserUpdateEvent(client, oldUser, newUser);
         });
     } else {
         if (options?.debug)
-            console.log('guildMemberUpdate, userUpdate event handlers not registered (missing GUILD_MEMBERS intents).');
+            console.log('[discord-logs] [DEBUG] guildMemberUpdate, userUpdate event handlers not registered (missing GUILD_MEMBERS intents).');
     }
 
     /* HANDLE MESSAGE UPDATE EVENTS */
     if (intents.has('GUILD_MESSAGES')) {
-        if (options?.debug) console.log('messageUpdate event handler registered.');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] messageUpdate event handler registered.');
         client.on('messageUpdate', (oldMessage, newMessage) => {
             handleMessageUpdateEvent(client, oldMessage, newMessage);
         });
     } else {
-        if (options?.debug) console.log('messageUpdate event handler not registered (missing GUILD_MESSAGES intent).');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] [discord-logs] [DEBUG] messageUpdate event handler not registered (missing GUILD_MESSAGES intent).');
     }
 
     /* HANDLE PRESENCE UPDATE EVENTS */
     if (intents.has('GUILD_PRESENCES')) {
-        if (options?.debug) console.log('presenceUpdate event handler registered.');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] presenceUpdate event handler registered.');
         client.on('presenceUpdate', (oldPresence, newPresence) => {
             handlePresenceUpdateEvent(client, oldPresence, newPresence);
         });
     } else {
         if (options?.debug)
-            console.log('presenceUpdate event handler not registered (missing GUILD_PRESENCES intent).');
+            console.log('[discord-logs] [DEBUG] presenceUpdate event handler not registered (missing GUILD_PRESENCES intent).');
     }
 
     /* HANDLE VOICE STATE UPDATE */
     if (intents.has('GUILD_VOICE_STATES')) {
-        if (options?.debug) console.log('voiceStateUpdate event handler registered.');
+        if (options?.debug) console.log('[discord-logs] [DEBUG] voiceStateUpdate event handler registered.');
         client.on('voiceStateUpdate', (oldState, newState) => {
             handleVoiceStateUpdateEvent(client, oldState, newState);
         });
     } else {
         if (options?.debug)
-            console.log('voiceStateUpdate event handler not registered (missing GUILD_VOICE_STATES intent).');
+            console.log('[discord-logs] [DEBUG] voiceStateUpdate event handler not registered (missing GUILD_VOICE_STATES intent).');
     }
 };
